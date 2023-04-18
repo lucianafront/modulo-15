03568214426ac4158e1ee671d7c10ab3c2def80f
Luciana de Souza Felix <luciana.quimicatec@outlook.com>
2023-04-18 14:48:41 -0300

    correção

--- Exercicio/script.js
+++ Exercicio/script.js
@@ -1,81 +1,145 @@
+
 var app = window.app || {}
+app.tudoValidado = true;
 
-app.validarEmail = function (elemento) {
+
+app.validarTelefoneBR = function () {
+
+    var resultado = document.getElementById('resultado');
+
+    var telefone = document.getElementById('telefone');
+    var msg_erro = document.getElementById('telefone_erro');
+
+    console.log(telefone.value)
+
+    var semErros = /^\(?\d{2}\)?[-.\s]?\d{4,5}[-.\s]?\d{4}$/.test(telefone.value);
+
+    /*
+    * // Exemplo de uso:
+        console.log(validarTelefoneBR("(31)3233-4343")); // Retorna true
+        console.log(validarTelefoneBR("31.3233.4343")); // Retorna true
+        console.log(validarTelefoneBR("31 32334343")); // Retorna true
+        console.log(validarTelefoneBR("(31)3233-4343 ramal 123")); // Retorna false
+        console.log(validarTelefoneBR("1234-5678")); // Retorna false
+    * */
+
+    if (semErros === false) {
+        app.tudoValidado = false
+        telefone.classList.add("erro")
+        msg_erro.style.display = 'block'
+    } else {
+        telefone.classList.remove("erro")
+        msg_erro.style.display = 'none'
+    }
+}
+
+
+
+
+
+
+
+app.validarTudo = function (elemento) {
     elemento.preventDefault();
+    app.tudoValidado = true;
+    
+    app.validarTelefoneBR();
 
-    let tudoValidado = true;
-    let mensagens=[];
+
 
 
     var nome = document.getElementById('nome');
     var email = document.getElementById('email');
-    var telefone = document.getElementById('telefone');
     var cep = document.getElementById('cep');
     var cidade = document.getElementById('cidade');
     var uf = document.getElementById('uf');
 
-
     if (nome.value === "") {
-        tudoValidado = false;
+        app.tudoValidado = false;
         nome.classList.add("erro");
-        
-   mensagens.push ("nome precisa ser digitado.")
+        var erro_nome = document.getElementById('nome_erro');
+        erro_nome.style.display = 'block'
+
     } else {
         nome.classList.remove("erro");
+        var erro_nome = document.getElementById('nome_erro');
+        erro_nome.style.display = 'none'
     }
 
-    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;   
-     if (email.value === ""  || !regexEmail.test(email.value)) {
-        tudoValidado = false;
+    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email.value === "" || !regexEmail.test(email.value)) {
+        app.tudoValidado = false;
         email.classList.add("erro");
-        mensagens.push ("email precisa ser digitado.")
+        
+        var email_erro= document.getElementById('email_erro');
+        email_erro.style.display = 'block'
     } else {
-        email.classList.remove("erro")
+        email.classList.remove("erro");
+        var erro_email = document.getElementById('email_erro');
+        erro_email.style.display = 'none'
+    
     }
-
-    var regex = new RegExp('^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$');
-    if (telefone.value === ""  || !regex.test(telefone.value)) {
-        tudoValidado = false
-        telefone.classList.add("erro")
-        mensagens.push ("telefone precisa ser digitado")
-    } else {
-        telefone.classList.remove("erro")
-    }
-
 
     var regex = new RegExp('^[0-9]{5}-[0-9]{3}$');
 
     if (cep.value === "" || !regex.test(cep.value)) {
-        tudoValidado = false
+        app.tudoValidado = false
         cep.classList.add("erro")
-        mensagens.push ("cep precisa ser digitado")
-    } else {
+
+        var cep_erro= document.getElementById('cep_erro');
+        cep_erro.style.display='block'
+    } 
+    else {
         cep.classList.remove("erro")
+        var cep_erro= document.getElementById('cep_erro');
+        cep_erro.style.display = 'none'
+
     }
 
     if (cidade.value === "") {
-        tudoValidado = false
+        app.tudoValidado = false
         cidade.classList.add("erro")
-        mensagens.push ("cidade precisa ser digitado")
-
-    } else {
+        var cidade_erro= document.getElementById('cidade_erro');
+        cidade_erro.style.display='block'
+    }
+     else {
         cidade.classList.remove("erro")
+        var cidade_erro= document.getElementById('cidade_erro');
+        cidade_erro.style.display = 'none'
+
+
     }
 
     const regexuf = /^(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)$/i;
 
     if (uf.value === "" || !regexuf.test(uf.value)) {
-        tudoValidado = false
-    uf.classList.add("erro")
-    mensagens.push ("cidade precisa ser digitado")
+        app.tudoValidado = false
+        uf.classList.add("erro")
+        var uf_erro= document.getElementById('uf_erro');
+        uf_erro.style.display='block'
     } else {
         uf.classList.remove("erro")
+        var uf_erro= document.getElementById('uf_erro');
+        uf_erro.style.display = 'none'
+
     }
 
 
     console.log(uf, cep, email, nome, telefone, cidade);
 
 
-    return tudoValidado;
+    var resultado = document.getElementById('resultado');
+    if(app.tudoValidado === true){
 
+        resultado.style.display = 'block';
+        app.limparFormulário();
+    }
+    else
+        resultado.style.display = 'none'
+
+}
+
+
+app.limparFormulário = function () {
+    document.getElementById("meuForm").reset();
 }
